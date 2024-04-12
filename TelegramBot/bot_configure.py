403ddbc4ba66c29e2022fa7e65fcddc89fdf8d6b
Zakateeeks <dmitrystrange2@gmail.com>
2024-04-12 16:03:00 +0500

    add ini

--- TelegramBot/bot_configure.py
+++ TelegramBot/bot_configure.py
@@ -1,8 +1,12 @@
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import configparser
 
+config = configparser.ConfigParser()
+config.read('../data.ini')
 storage = MemoryStorage()
-API_TOKEN = ""
+API_TOKEN = config["BOT"]["BOT_API"]
+print(API_TOKEN)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot, storage=storage)
